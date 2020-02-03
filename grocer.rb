4e346dd2ba2f0c9cf42924539b01de569a14eefb
John Souza <souzajohnlee@gmail.com>
2020-02-03 18:52:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,7 +60,8 @@ def apply_clearance(cart)
   #
   # REMEMBER: This method **should** update cart
   cart.each do |item|
-    item[:clearance] ? 
+    item[:clearance] ? item[:price] = item[:price] * 0.2 : nil
+  end
 end
 
 def checkout(cart, coupons)
